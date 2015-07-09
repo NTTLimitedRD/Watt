0a216e646e3660b8a823cb125a9a8fa94e3d9c50
Adam Friedman <adam.friedman@itaas.dimensiondata.com>
2015-07-09 14:11:03 +1000

    Target net45 instead of net451.

--- SolutionAssemblyInfo.cs
+++ SolutionAssemblyInfo.cs
@@ -13,5 +13,5 @@
 #endif // DEBUG
 
 [assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
+[assembly: AssemblyInformationalVersion("0.1.1.0")]
