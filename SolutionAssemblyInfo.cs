a85108af1b95563c837ed186a3a98476e4e0bc75
tintoy <tintoy@tintoy.io>
2015-10-01 15:16:13 +1000

    Fix file version to be in sync with package version.
    (yesyesyes, I'd set up a proper automated versioning system if I had the time)

--- SolutionAssemblyInfo.cs
+++ SolutionAssemblyInfo.cs
@@ -12,6 +12,6 @@
 [assembly: AssemblyConfiguration("")]
 #endif // DEBUG
 
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
-[assembly: AssemblyInformationalVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.1")]
+[assembly: AssemblyInformationalVersion("1.0.3.1")]
